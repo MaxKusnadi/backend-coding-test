54906d6b8db0eb0342875476f388f3c0195839e2
Max Kusnadi <max.kusnadi@gmail.com>
2019-11-29 01:36:02 +0800

    increase coverage

--- tests/api.test.js
+++ tests/api.test.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const request = require('supertest');
+const assert = require('assert');
 
 const sqlite3 = require('sqlite3').verbose();
 const db = new sqlite3.Database(':memory:');
@@ -9,24 +10,227 @@ const app = require('../src/app')(db);
 const buildSchemas = require('../src/schemas');
 
 describe('API tests', () => {
-    before((done) => {
-        db.serialize((err) => { 
-            if (err) {
-                return done(err);
-            }
+  before((done) => {
+    db.serialize((err) => {
+      if (err) {
+        return done(err);
+      }
 
-            buildSchemas(db);
+      buildSchemas(db);
 
-            done();
-        });
+      done();
+    });
+  });
+
+  describe('GET /health', () => {
+    it('should return health', (done) => {
+      request(app)
+          .get('/health')
+          .expect('Content-Type', /text/)
+          .expect(200, done);
+    });
+  });
+
+  describe('GET /rides', () => {
+    it('should return not found', (done) => {
+      request(app)
+          .get('/rides')
+          .expect(200)
+          .expect((res) => {
+            assert(res.body.error_code === 'RIDES_NOT_FOUND_ERROR');
+          })
+          .end(done);
     });
 
-    describe('GET /health', () => {
-        it('should return health', (done) => {
+    it('should return server error', (done) => {
+      request(app)
+          .get('/rides')
+          .expect(200)
+          .expect((res) => {
+            assert(res.body.error_code === 'RIDES_NOT_FOUND_ERROR');
+          })
+          .end(done);
+    });
+
+    it('should return fine', (done) => {
+      request(app)
+          .post('/rides')
+          .send({
+            'start_long': 100,
+            'start_lat': 70,
+            'end_long': 110,
+            'end_lat': 75,
+            'rider_name': 'Max',
+            'driver_name': 'John',
+            'driver_vehicle': 'Car',
+          })
+          .set('Content-Type', 'application/json')
+          .expect(200)
+          .then((res) => {
             request(app)
-                .get('/health')
-                .expect('Content-Type', /text/)
-                .expect(200, done);
-        });
+                .get(`/rides`)
+                .expect(200)
+                .expect((getResponse) => {
+                  assert(getResponse.body[0].rideID === res.body.rideID);
+                })
+                .end(done);
+          });
     });
+  });
+
+  describe('GET /rides/{id}', () => {
+    it('should return not found', (done) => {
+      request(app)
+          .get('/rides/99')
+          .expect(200)
+          .expect((res) => {
+            assert(res.body.error_code === 'RIDES_NOT_FOUND_ERROR');
+          })
+          .end(done);
+    });
+
+    it('should return fine', (done) => {
+      request(app)
+          .post('/rides')
+          .send({
+            'start_long': 100,
+            'start_lat': 70,
+            'end_long': 110,
+            'end_lat': 75,
+            'rider_name': 'Max',
+            'driver_name': 'John',
+            'driver_vehicle': 'Car',
+          })
+          .set('Content-Type', 'application/json')
+          .expect(200)
+          .then((res) => {
+            request(app)
+                .get(`/rides/${res.body.rideID}`)
+                .expect(200)
+                .expect((getResponse) => {
+                  // console.log(getResponse);
+                  assert(getResponse.body.rideID === res.body.rideID);
+                })
+                .end(done);
+          });
+    });
+  });
+
+  describe('POST /rides', () => {
+    it('should create ride successfully', (done) => {
+      request(app)
+          .post('/rides')
+          .send({
+            'start_long': 100,
+            'start_lat': 70,
+            'end_long': 110,
+            'end_lat': 75,
+            'rider_name': 'Max',
+            'driver_name': 'John',
+            'driver_vehicle': 'Car',
+          })
+          .set('Content-Type', 'application/json')
+          .expect(200)
+          .expect((res) => {
+            assert(res.body.rideID != null);
+          })
+          .end(done);
+    });
+
+    it('should fail with invalid initial position', (done) => {
+      request(app)
+          .post('/rides')
+          .send({
+            'start_long': 200,
+            'start_lat': 200,
+            'end_long': 110,
+            'end_lat': 75,
+            'rider_name': 'Max',
+            'driver_name': 'John',
+            'driver_vehicle': 'Car',
+          })
+          .set('Content-Type', 'application/json')
+          .expect((res) => {
+            assert(res.body.error_code === 'VALIDATION_ERROR');
+          })
+          .end(done);
+    });
+
+    it('should fail with invalid final position', (done) => {
+      request(app)
+          .post('/rides')
+          .send({
+            'start_long': 100,
+            'start_lat': 70,
+            'end_long': 200,
+            'end_lat': 200,
+            'rider_name': 'Max',
+            'driver_name': 'John',
+            'driver_vehicle': 'Car',
+          })
+          .set('Content-Type', 'application/json')
+          .expect((res) => {
+            assert(res.body.error_code === 'VALIDATION_ERROR');
+          })
+          .end(done);
+    });
+
+    it('should fail with invalid rider\'s name', (done) => {
+      request(app)
+          .post('/rides')
+          .send({
+            'start_long': 100,
+            'start_lat': 70,
+            'end_long': 110,
+            'end_lat': 75,
+            'rider_name': 123,
+            'driver_name': 'John',
+            'driver_vehicle': 'Car',
+          })
+          .set('Content-Type', 'application/json')
+          .expect((res) => {
+            assert(res.body.error_code === 'VALIDATION_ERROR');
+          })
+          .end(done);
+    });
+
+    it('should fail with invalid driver\'s name', (done) => {
+      request(app)
+          .post('/rides')
+          .send({
+            'start_long': 100,
+            'start_lat': 70,
+            'end_long': 110,
+            'end_lat': 75,
+            'rider_name': 'Max',
+            'driver_name': 123,
+            'driver_vehicle': 'Car',
+          })
+          .set('Content-Type', 'application/json')
+          .expect((res) => {
+            assert(res.body.error_code === 'VALIDATION_ERROR');
+          })
+          .end(done);
+    });
+
+    it('should fail with invalid driver\'s name', (done) => {
+      request(app)
+          .post('/rides')
+          .send({
+            'start_long': 100,
+            'start_lat': 70,
+            'end_long': 110,
+            'end_lat': 75,
+            'rider_name': 'Max',
+            'driver_name': 'John',
+            'driver_vehicle': 123,
+          })
+          .set('Content-Type', 'application/json')
+          .expect((res) => {
+            assert(res.body.error_code === 'VALIDATION_ERROR');
+          })
+          .end(done);
+    });
+  });
 });
+
