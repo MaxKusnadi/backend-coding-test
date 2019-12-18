31658243b472a4004908129f8f63bd8db8783886
Max Kusnadi <max.kusnadi@gmail.com>
2019-12-19 01:01:14 +0800

    add error generator class

--- src/constants.js
+++ src/constants.js
@@ -1,5 +1,36 @@
 'use strict';
 
+/**
+ * Possible error given by the app
+ * @type {{SERVER_ERROR: string, NOT_FOUND: string, VALIDATION_ERROR: string}}
+ */
+const ERROR_CODE = {
+  VALIDATION_ERROR: 'VALIDATION_ERROR',
+  SERVER_ERROR: 'SERVER_ERROR',
+  NOT_FOUND: 'RESOURCE_NOT_FOUND',
+};
+
+/**
+ * Possible HTTP code responses given by the app
+ *
+ * @type {{CREATED: number, SERVER_ERROR: number,
+ * SUCCESSFUL: number, NOT_FOUND: number, VALIDATION_ERROR: number}}
+ */
+const HTTP_CODE = {
+  VALIDATION_ERROR: 400,
+  SERVER_ERROR: 500,
+  NOT_FOUND: 404,
+  SUCCESSFUL: 200,
+  CREATED: 201,
+};
+
+/**
+ * Collection of various DB scripts used in the app
+ *
+ * @type {{createTable: (function(*, *): string),
+ * getRideById: (function(*): string), getAllRides: (function(): string),
+ * createRide: (function(): string)}}
+ */
 const DB_SCRIPTS = {
   createTable: (tableName, tableContent) => `CREATE TABLE ${tableName}`+
         `(${tableContent})`,
@@ -12,4 +43,6 @@ const DB_SCRIPTS = {
 
 module.exports = {
   DB_SCRIPTS,
+  HTTP_CODE,
+  ERROR_CODE,
 };
